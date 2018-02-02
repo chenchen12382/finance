78b46d20c1f0801711e79557d749ab937057e5ed
chenchen12382 <chenchen12382@163.com>
2018-02-02 18:01:49 +0800

    redis

---
+++ src/test/java/com/pz/TestRedis.java
@@ -0,0 +1,75 @@
+package com.pz;
+
+import com.pz.model.User;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.test.context.junit4.SpringRunner;
+import redis.clients.jedis.Jedis;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Administrator on 2018/2/2.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestRedis {
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void test() throws Exception {
+        //连接本地的 Redis 服务
+        Jedis jedis = new Jedis("localhost");
+        System.out.println("连接成功");
+        //查看服务是否运行
+        System.out.println("服务正在运行: "+jedis.ping());
+
+        //连接本地的 Redis 服务
+//        Jedis jedis = new Jedis("localhost");
+        System.out.println("连接成功");
+        //设置 redis 字符串数据
+        jedis.set("runoobkey", "www.runoob.com");
+        // 获取存储的数据并输出
+        System.out.println("redis 存储的字符串为: "+ jedis.get("runoobkey"));
+
+        //存储数据到列表中
+        jedis.lpush("site-list", "Runoob");
+        jedis.lpush("site-list", "Google");
+        jedis.lpush("site-list", "Taobao");
+        // 获取存储的数据并输出
+        List<String> list = jedis.lrange("site-list", 0 ,2);
+        for(int i=0; i<list.size(); i++) {
+            System.out.println("列表项为: " + list.get(i));
+        }
+
+    }
+
+//    @Test
+//    public void testObj() throws Exception {
+//        User user=new User("aa@126.com", "aa", "aa123456", "aa","123");
+//        ValueOperations<String, User> operations=redisTemplate.opsForValue();
+//        operations.set("com.neox", user);
+//        operations.set("com.neo.f", user,1, TimeUnit.SECONDS);
+//        Thread.sleep(1000);
+//        //redisTemplate.delete("com.neo.f");
+//        boolean exists=redisTemplate.hasKey("com.neo.f");
+//        if(exists){
+//            System.out.println("exists is true");
+//        }else{
+//            System.out.println("exists is false");
+//        }
+//        // Assert.assertEquals("aa", operations.get("com.neo.f").getUserName());
+//    }
+}
